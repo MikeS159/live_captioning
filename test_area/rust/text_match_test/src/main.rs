455cc2d62e5750826d8808cf9ee210e819bb8936
Mike Simpson <michael.simpson1@uk.l3harris.com>
2025-05-27 16:07:23 +0100

    Testing different algorithms

--- test_area/rust/text_match_test/src/main.rs
+++ test_area/rust/text_match_test/src/main.rs
@@ -1,11 +1,9 @@
-use rapidfuzz::distance::levenshtein;
-use rapidfuzz::distance::prefix;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::collections::BinaryHeap;
-use ordered_float::OrderedFloat;
-use std::cmp::{Ordering, Reverse};
+use std::cmp::{Ordering};
 use textdistance::{Algorithm, SorensenDice};
+use std::time::Instant;
 
 #[derive(PartialEq, PartialOrd, Debug)]
 struct OrderedF64(f64);
@@ -19,26 +17,38 @@ impl Ord for OrderedF64 {
 }
 
 fn main() -> io::Result<()> {
-    let file = File::open("Conversation.txt")?;
-    let reader = io::BufReader::new(file);
+    let script_file = File::open("C:\\Users\\u290676\\Repos\\Github\\live_captioning\\test_area\\script_noname.txt")?;
+    let gen_file = File::open("C:\\Users\\u290676\\Repos\\Github\\live_captioning\\test_area\\output_wav.txt")?;
+    let script_reader = io::BufReader::new(script_file);
+    let gen_reader = io::BufReader::new(gen_file);
 
-    let lines: Vec<String> = reader.lines().collect::<Result<_, _>>()?;
+    let script_lines: Vec<String> = script_reader.lines().collect::<Result<_, _>>()?;
+    let gen_lines: Vec<String> = gen_reader.lines().collect::<Result<_, _>>()?;
     let mut heap = BinaryHeap::new();
 
-    let s = "You mean the bimpy";// code? The binary multiply?";
-    for line in &lines {
-        let a = SorensenDice::default();
-        let r = a.for_str(s, line);
-        //println!("{} - {}", r.nval(), line);
-        heap.push(Reverse((OrderedF64(r.nval()), line)));
-        //println!("{} - {}", l, line);
-    }
+    let start = Instant::now();
 
-    // Print in descending order (BinaryHeap is a max heap by default)
-    while let Some(Reverse((OrderedF64(l), line))) = heap.pop() {
-        println!("{}: {}", l, line);
+    for gen_line in &gen_lines {
+        //println!("Searching for line - {}", gen_line);
+        for script_line in &script_lines {
+            let a = SorensenDice::default();
+            let r = a.for_str(gen_line, script_line);
+            //println!("{} - {}", r.nval(), line);
+            heap.push((OrderedF64(r.nval()), script_line));
+        }
+        // Print in descending order (BinaryHeap is a max heap by default)
+        for _ in 0..5 {
+            if let Some((OrderedF64(l), script_line)) = heap.pop() {
+                //println!("{}: {}", l, script_line);
+            } else {
+                break;
+            }
+        }
+        //let mut line = String::new();
+        //std::io::stdin().read_line(&mut line)?;
     }
-
+    let duration = start.elapsed();
+    println!("Execution time: {:.2?}", duration);
     Ok(())
 }
 
