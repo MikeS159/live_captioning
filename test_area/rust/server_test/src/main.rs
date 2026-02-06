cf97b06bda084e754a2ce4894e6e2d8494bd0119
Mike Simpson <mike_simpson_159@outlook.com>
2026-02-06 23:45:23 +0000

    Video display and fixed Speakers
    Videos can now be displayed below text and must be explicitly cleared (not working?). Fixed speakers to match style json file.

--- test_area/rust/server_test/src/main.rs
+++ test_area/rust/server_test/src/main.rs
@@ -149,7 +149,15 @@ async fn main() {
     let speaker_styles = load_speaker_styles("src/speaker_styles.json");
     // broadcast channel for pushing LineMessage to all connected clients.
     let (tx, _rx) = broadcast::channel::<LineMessage>(16);
-    let mut lines = load_lines_from_file("src/00_prologue.json");
+    let mut lines = Vec::new();
+    for i in 0..=15 {
+        let file = if i == 0 {
+            format!("src/00_prologue.json")
+        } else {
+            format!("src/{:02}_scene{}.json", i, i)
+        };
+        lines.extend(load_lines_from_file(&file));
+    }
     //let mut lines = load_lines_from_file("src/01_scene1.json");
 
     // Apply default style if missing
